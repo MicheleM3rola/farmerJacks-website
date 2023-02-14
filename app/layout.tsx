581fce868ed06214a65411e232d2497ea6f8b68f
Mike <mike88.merola@gmail.com>
2023-02-14 16:43:27 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Navbar from './nav'
 
 export default function RootLayout({
   children,
@@ -11,8 +12,12 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+      
       <head />
-      <body>{children}</body>
+      <body>
+      <Navbar/>
+        {children}
+        </body>
     </html>
   )
 }
---
+++ app/workwithus/page.tsx
@@ -0,0 +1,7 @@
+export default function WorkWithUs(){
+    return(
+        <main>
+            <h1>Work with us</h1>
+        </main>
+    )
+}
