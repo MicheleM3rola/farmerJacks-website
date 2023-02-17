182964d995d02947e33f3c5fdb530f1298540e05
Mike <mike88.merola@gmail.com>
2023-02-17 20:28:19 +0800

    working on template and header

--- app/hero/Hero.tsx
+++ app/hero/Hero.tsx
@@ -7,7 +7,7 @@ import heroImage from "../../public/headerbg.jpg"
 export default function Hero(){
     return(
         <header className="min-h-screen flex flex-col justify-evenly items-center w-full">
-            <Image src={heroImage} className="absolute h-full w-full -z-20 object-cover aspect-auto object-center opacity-70 " alt="Hero Image"   />
+            <Image src={heroImage} className="absolute h-full w-full -z-20 object-cover aspect-auto object-center " alt="Hero Image"   />
             <h1 className="text-black text-6xl font-bold">Farmer Jacks</h1>
         </header>
     )
