525ead677e6e2d169a6954ac741d43e03051a82c
Mike <mike88.merola@gmail.com>
2023-02-19 21:28:08 +0800

    responsive nav added

--- app/hero/Hero.tsx
+++ app/hero/Hero.tsx
@@ -9,8 +9,8 @@ export default function Hero(){
     return(
         <header className="min-h-screen flex flex-col justify-evenly items-center w-full">
             
-            <div className="absolute bg-gradient-to-t inset-0 -bottom-32 top-1/3 from-bkgpic to-transparent"></div>
-            <Image src={heroImage} className="absolute h-full w-full -z-20 object-cover aspect-auto object-center " alt="Hero Image"   />
+            <div className="absolute bg-gradient-to-t inset-0 -bottom-32 top-2/3 md:top-1/3 from-bkgpic to-transparent"></div>
+            <Image src={heroImage} className="absolute h-full w-full -z-20 object-cover aspect-auto object-center " alt="Hero Image" />
             <div className="w-5/6 flex flex-col justify-end items-end gap-3">
                 <h1 className="text-black text-7xl font-cinzelFont font-normal">Farmer Jack's</h1>
                 <small className="text-2xl font-cinzelFont font-normal text-green-600">Forrestfield</small>
--- app/navigation/nav.tsx
+++ app/navigation/nav.tsx
@@ -1,21 +1,42 @@
 'use client'
+import { useState } from "react"
 import Link from "next/link"
 import Image from "next/image"
 import FjLogo from '../../public/fjcentro.jpg'
 
 
+
+
 export default function Navbar(){
+    const[toggleClass,setToggleClass]=useState(false)
 
     return(
-        <nav className="w-full  p-6 flex justify-center items-center absolute z-50">
-            <div className="w-5/6 flex items-center justify-between">
-              <div className="w-full flex justify-end">
-              <Image src={FjLogo} alt="Logo" width={80} height={80} className="rounded-lg"/>
+        <nav className="flex justify-between items-center lg:absolute w-[92%] mx-auto z-50 md:pt-5">
+            <div className="w-full flex justify-start md:justify-end md:mr-12 ">
+                
+                    <Image src={FjLogo} alt="Logo" width={68} height={68} className="rounded-lg"/>
+                
+              
               
 
               </div>
-              <div className="w-full">
-                <ul className="w-full flex items-center justify-end gap-5 cursor-pointer text-black text-lg font-cinzelFont font-normal">
+              <div className="flex items-center gap-6 lg:hidden">
+                    <button className="cursor-pointer" onClick={()=>{
+                        setToggleClass(!toggleClass)
+                    }}>
+
+                        {toggleClass ? (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                         </svg>) : 
+                                         (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                           </svg>)}
+                    </button>
+              </div>
+            
+             
+              <div className={  `bg-bkgpic lg:bg-transparent duration-500 lg:static absolute lg:min-h-fit min-h-[60vh] ${toggleClass ? 'left-0': 'left-[-100%]'} top-[8%] w-full flex items-center ` }>
+                <ul className="w-full flex lg:flex-row flex-col items-center lg:justify-end  gap-5 cursor-pointer text-black text-base lg:text-sm xl:text-lg  font-cinzelFont font-normal">
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/about">About</Link></li>
                     <li><Link href="/workwithus">Work with us</Link></li>
@@ -23,8 +44,15 @@ export default function Navbar(){
                     <li className="underline underline-offset-8 decoration-green-500">Catalogue</li>
                 </ul>
               </div>
-            </div>
+
+            
+
+            
             
         </nav>
     )
 }
+
+
+
+
