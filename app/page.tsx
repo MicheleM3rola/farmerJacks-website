5781aa2e40d9177a1bf6d4982f2327339fff98b3
Mike <mike88.merola@gmail.com>
2023-02-19 21:35:43 +0800

    starting different section

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,11 @@ export default function Home() {
     <div className='flex flex-col items-center justify-center'>
       <Hero/>
      <main className='relative mt-16 sm:mt-24 lg:mt-40 pb-16 -mb-16 grid gap-16 sm:gap-24 lg:gap-40 overflow-hidden bg-bkgpic'>
-
+          <section>
+           <small>At Centro</small>
+           <h2> Indipendent Farmer Jack's</h2>
+           <p> An Indipendent Family Bussiness situated in Edinburg Road 51 Forrestfield offering the best prices without compromise </p>
+          </section>
      </main>
      
     </div>
