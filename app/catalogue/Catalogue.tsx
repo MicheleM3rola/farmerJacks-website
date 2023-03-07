44899471c9eacf69a85e0d2a817a8d6b93cc2afb
Mike <mike88.merola@gmail.com>
2023-03-08 06:09:44 +0800

    working on catalogue

--- app/catalogue/Catalogue.tsx
+++ app/catalogue/Catalogue.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { ImagesData } from "@/utils";
 import CatalogueSlider from "./CatalogueSlider";
 
-import logo from '../../public/fjBan.jpg'
+import logo from '../../public/fjCentro.jpg'
 
 const specials=[
     {
@@ -46,11 +46,22 @@ const specials=[
 export default function Catalogue(){
 
     return(
-        <section className="flex flex-col justify-center items-center w-full bg-logoColor">
-           
-            <h1 className=" text-5xl font-cinzelFont mb-20 mt-20 text-white">Welcome to Farmer Jack's Forrestield</h1>
-            <div className="flex flex-col justify-center items-center w-full ">
-            <div className="w-full h-full flex flex-wrap justify-center items-center gap-2">
+        <section className="flex flex-col justify-center items-center w-full catalogue">
+            <div className="w-full flex justify-center items-center gap-3 mt-20">
+            <Image src={logo} alt="logo width={60}"width={90} height={90} className="rounded-lg border border-solid border-white "/>
+            <h1 className=" text-5xl font-cinzelFont  text-white">Welcome to Farmer Jack's Forrestield</h1>
+
+            </div>
+            
+            <div className=" flex justify-center items-center">
+            <CatalogueSlider/>
+            <div className="flex flex-col justify-center items-start text-white">
+            <h2 className="text-5xl py-7 font-cinzelFont">Weekly Catalogue</h2>
+            <p className="text-lg w-1/2 mb-2 font-cinzelFont">The Catalogue contain  the specials for the week, for the others,come and see us and you won't regret it</p>
+            <button className="btn btn-active font-cinzelFont">Download</button>
+            </div>
+            </div>
+            <div className="w-full h-full flex flex-wrap justify-center items-center gap-2 pb-16">
             
            {
             specials.map((special,index)=>{
@@ -67,16 +78,9 @@ export default function Catalogue(){
            }
             
             </div>
-            <div className="mt-28 flex ">
-            <CatalogueSlider/>
-            <div className="flex flex-col justify-center items-start text-white">
-            <h2 className="text-5xl py-7 font-cinzelFont">Weekly Catalogue</h2>
-            <p className="text-lg w-1/2 mb-2 font-cinzelFont">The Catalogue contain  the specials for the week, for the others,come and see us and you won't regret it</p>
-            <button className="btn btn-active font-cinzelFont">Download</button>
-            </div>
-            </div>
+           
             
-            </div>
+            
         </section>
     )
 }
